215f7d407a46ea7c6160301b92189c759fa7edd5
parinita.sharda@crowdbotics.com <parinita.sharda@crowdbotics.com>
2023-05-29 18:11:31 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "gateway-41065",
-  url: "https://gateway-41065.botics.co",
-  api: "https://gateway-41065.botics.co/api/v1"
-}
+  "name": "gateway-41065",
+  "url": "https://gateway-41065.botics.co",
+  "api": "https://gateway-41065.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
